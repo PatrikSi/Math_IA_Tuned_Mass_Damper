bfb4c767c6773b358677f848b374afedc97a7d45
Patrik Sivak <patrik.sivak.314@gmail.com>
2022-08-21 11:04:28 +0200

    commit

--- spring_mass_damper_system.py
+++ spring_mass_damper_system.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
+import math
 import cmath
 import numpy as np
-from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+from sympy import symbols, Eq
+
+time = np.linspace(0, 20)
+initial_conditions = [4, 0]
 
 
 def solve_second_order_system(
@@ -17,5 +21,27 @@ def solve_second_order_system(
     sol1 = (-b - cmath.sqrt(d)) / (2 * m)
     sol2 = (-b + cmath.sqrt(d)) / (2 * m)
 
+    return {'real': sol1.real, 'imag': abs(sol1.imag)}
 
-solve_second_order_system(1, 1, 1)
+
+coefficients = solve_second_order_system(m=2,
+                                         b=0.025,
+                                         k=-5,
+                                         )
+
+
+def create_equation(
+        t,
+        real: float,
+        imag: float,
+):
+    A = 1
+    B = 1
+    print(real)
+    plt.plot(t, (math.e ** (real * t)) * (A*np.sin(imag * t) + B*np.cos(imag * t)))
+    plt.show()
+    return (math.e ** (real * t)) * (A*np.sin(imag * t) + B*np.cos(imag * t))
+
+
+print(create_equation(time, **coefficients))
+
