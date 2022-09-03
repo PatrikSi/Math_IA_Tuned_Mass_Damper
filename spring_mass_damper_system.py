c976781fb9ea6ed85ce61f9017e9714c1379e161
Patrik Sivak <patrik.sivak.314@gmail.com>
2022-09-03 14:16:22 +0200

    commit

--- spring_mass_damper_system.py
+++ spring_mass_damper_system.py
@@ -2,8 +2,8 @@ import numpy as np
 from scipy import integrate
 import matplotlib.pyplot as plt
 
-b = 1
-k = 2
+b = 4
+k = 4
 initial = [0, 4]
 
 
@@ -14,7 +14,7 @@ def solver(Y, t):
 a_t = np.arange(0, 25.0, 0.01)
 asol = integrate.odeint(solver, initial, a_t)
 print(asol)
-plt.title(f'Only building, mass normalized, b={b}, k={k}')
+plt.title(f'b={b}, k={k}, maximum displacement={round(np.amax(asol[:, 0]), 3)}')
 plt.plot(asol[:, 0], label='position')
 plt.plot(asol[:, 1], label='velocity')
 plt.xlim(0, 1000)
